3ca008b3c9d09a3e27ee301aa6272327fc9d8f39
park-hyunjung <148519637+park-hyunjung@users.noreply.github.com>
2024-10-09 14:51:02 +0900

    Add files via upload

---
+++ Example_4_7_3SW_LED_Buzzer_Interrupt_select.py
@@ -0,0 +1,198 @@
+import os
+import time
+import select
+
+# GPIO 핀 번호 리스트
+GPIO_PINS = [83, 84, 112, 113, 117, 118, 120, 121]
+GPIO_PINS_1 = [83, 112, 117, 120]
+GPIO_PINS_2 = [84, 113, 118, 121]
+BUZZER_PIN = 89
+SWITCH_PIN_1 = 65
+SWITCH_PIN_2 = 66
+SWITCH_PIN_3 = 85
+
+switch1_value_pre = 0
+switch1_value_cur = 0
+switch1_event_flag = 0
+
+switch2_value_pre = 0
+switch2_value_cur = 0
+switch2_event_flag = 0
+
+switch3_value_pre = 0
+switch3_value_cur = 0
+switch3_event_flag = 0
+
+index = 0
+
+# GPIO 초기화 함수
+def gpio_setup_output(pin):
+    # GPIO 핀을 내보내기 (export)
+    with open("/sys/class/gpio/export", "w") as f:
+        f.write(str(pin))
+    
+    # 핀 모드 설정 (출력)
+    with open(f"/sys/class/gpio/gpio{pin}/direction", "w") as f:
+        f.write("out")
+
+def gpio_setup_input(pin):
+    # GPIO 핀을 내보내기 (export)
+    with open("/sys/class/gpio/export", "w") as f:
+        f.write(str(pin))
+    
+    # 핀 모드 설정 (출력)
+    with open(f"/sys/class/gpio/gpio{pin}/direction", "w") as f:
+        f.write("in")
+
+# GPIO 상태 쓰기 함수
+def gpio_write(pin, value):
+    with open(f"/sys/class/gpio/gpio{pin}/value", "w") as f:
+        f.write(str(value))
+
+def gpio_read(pin):
+    if os.path.exists(f'/sys/class/gpio/gpio{pin}'):
+        with open(f'/sys/class/gpio/gpio{pin}/value', 'r') as f:
+            return int(f.read().strip())
+
+# GPIO 초기화 및 모든 핀 설정
+def setup_all_pins():
+    for pin in GPIO_PINS:
+        gpio_setup_output(pin)
+    gpio_setup_output(BUZZER_PIN)
+
+    gpio_setup_input(SWITCH_PIN_1)
+    gpio_setup_input(SWITCH_PIN_2)
+    gpio_setup_input(SWITCH_PIN_3)
+        
+# GPIO 해제 함수
+# GPIO 핀이 이미 export되어 있는지 확인하고, 그렇다면 unexport
+def gpio_cleanup(pin):
+    if os.path.exists(f"/sys/class/gpio/gpio{pin}"):
+        with open("/sys/class/gpio/unexport", "w") as f:
+            f.write(str(pin))
+
+# 모든 GPIO 해제
+def cleanup_all_pins():
+    for pin in GPIO_PINS:
+        gpio_cleanup(pin)
+
+    gpio_cleanup(BUZZER_PIN)
+    gpio_cleanup(SWITCH_PIN_1)
+    gpio_cleanup(SWITCH_PIN_2)
+    gpio_cleanup(SWITCH_PIN_3)
+
+try:
+    cleanup_all_pins()
+    setup_all_pins()
+
+    print("Switch Push 대기 상태...")
+    while True:
+        switch1_value_cur = gpio_read(SWITCH_PIN_1)
+        switch2_value_cur = gpio_read(SWITCH_PIN_2)
+        switch3_value_cur = gpio_read(SWITCH_PIN_3)
+
+        if switch1_value_cur == 0:
+            if switch1_value_pre == 1:  # falling edge detect
+                time.sleep(0.02)  # 바운스 방지
+
+                print("Switch 1 눌림")
+                if switch1_event_flag == 0:
+                    switch1_event_flag = 1
+                    switch2_event_flag = 0
+                    switch3_event_flag = 0
+                    print("LED Mode 1 동작 Start")
+                index = 0
+
+                for pin in GPIO_PINS:
+                    gpio_write(pin, 0)
+        switch1_value_pre = switch1_value_cur
+
+        if switch2_value_cur == 0:
+            if switch2_value_pre == 1:  # falling edge detect
+                time.sleep(0.02)  # 바운스 방지
+
+                print("Switch 2 눌림")
+                if switch2_event_flag == 0:
+                    switch1_event_flag = 0
+                    switch2_event_flag = 1
+                    switch3_event_flag = 0
+                    print("LED Mode 2 동작 Start")
+                index = 7
+
+                for pin in GPIO_PINS:
+                    gpio_write(pin, 0)
+        switch2_value_pre = switch2_value_cur
+
+        if switch3_value_cur == 0:
+            if switch3_value_pre == 1:  # falling edge detect
+                time.sleep(0.02)  # 바운스 방지
+
+                print("Switch 3 눌림")
+                if switch3_event_flag == 0:
+                    switch1_event_flag = 0
+                    switch2_event_flag = 0
+                    switch3_event_flag = 1
+                    print("LED Mode 3 동작 Start")
+
+                for pin in GPIO_PINS:
+                    gpio_write(pin, 0)
+        switch3_value_pre = switch3_value_cur
+
+        if switch1_event_flag == 1:
+            gpio_write(GPIO_PINS[index], 1)
+            time.sleep(0.5)
+            pre_pin = GPIO_PINS[index]
+            gpio_write(pre_pin, 0)
+
+            index = index +1
+            if index == 8:
+                index = 0
+        
+        if switch2_event_flag == 1:
+            gpio_write(GPIO_PINS[index], 1)
+            time.sleep(0.5)
+            pre_pin = GPIO_PINS[index]
+            gpio_write(pre_pin, 0)
+
+            index = index -1
+            if index == -1:
+                index = 7
+
+        if switch3_event_flag == 1:
+            gpio_write(BUZZER_PIN, 1)
+            print("대~~")
+            time.sleep(1)
+            gpio_write(BUZZER_PIN, 0)
+            time.sleep(0.1)
+
+            gpio_write(BUZZER_PIN, 1)
+            print("한!")
+            time.sleep(0.4)
+            gpio_write(BUZZER_PIN, 0)
+            time.sleep(0.1)
+
+            gpio_write(BUZZER_PIN, 1)
+            print("민!")
+            time.sleep(0.3)
+            gpio_write(BUZZER_PIN, 0)
+            time.sleep(0.1)
+
+            gpio_write(BUZZER_PIN, 1)
+            print("국!")
+            time.sleep(0.3)
+            gpio_write(BUZZER_PIN, 0)
+            time.sleep(0.1)
+
+            print("짝짝짝~ 짝! 짝!")
+
+            switch3_event_flag = 0
+
+        print("Switch Push 대기 상태...")
+
+except KeyboardInterrupt:
+    for pin in GPIO_PINS:
+        gpio_write(pin, 0)
+    print("프로그램 종료")
+
+finally:
+    cleanup_all_pins()
